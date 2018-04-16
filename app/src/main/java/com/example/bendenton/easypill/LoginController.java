ddaedae0e17096b12f7fdf7a3dac55b889142422
Benjamin Denton <bcdent4181@ung.edu>
2018-04-16 14:54:55 -0400

    Changed Pill

--- app/src/main/java/com/example/bendenton/easypill/LoginController.java
+++ app/src/main/java/com/example/bendenton/easypill/LoginController.java
@@ -2,12 +2,18 @@ package com.example.bendenton.easypill;
 
 import android.content.Intent;
 import android.net.Uri;
+import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
 public class LoginController
 {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view);
+    }
+
     public void touchLogin(View view) {
         EditText username = (EditText) findViewById(R.id.txtUsername);
         EditText password =  (EditText) findViewById(R.id.txtPassword);
@@ -22,4 +28,6 @@ public class LoginController
         {
             Toast.makeText(this, "Wrong username or password", Toast.LENGTH_LONG).show();
         }
+
+    }
 }

4c247ee606fa8d5ec8ffbc6345c590216917d875
Benjamin Denton <bcdent4181@ung.edu>
2018-04-16 23:26:31 -0400

    Added Pill stuff

--- app/src/main/java/com/example/bendenton/easypill/PillsController.java
+++ app/src/main/java/com/example/bendenton/easypill/PillsController.java
@@ -1,11 +1,42 @@
 package com.example.bendenton.easypill;
 
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 
-public class PillsController extends AppCompatActivity {
+public class PillsController extends AppCompatActivity
+{
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener()
+    {
+
+
+        public boolean onNavigationItemSelected(@NonNull MenuItem item)
+        {
+            switch (item.getItemId()) {
+                case R.id.navigation_pastPills:
+                    setTitle("Pills Taken");
+                    return true;
+                case R.id.navigation_futurePills:
+                    setTitle("Upcoming Pills");
+                    return true;
+            }
+            return false;
+        }
+
+    };
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.pill_layout);
+
+
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
 }
